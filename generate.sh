6072e8faaa83b741369d9321189d921f2873ed47
sunfmin <sunfmin@gmail.com>
2020-09-14 15:59:05 +0800

    install gqlgen_data_gen if don't exists

--- generate.sh
+++ generate.sh
@@ -2,4 +2,9 @@ root=$(pwd)
 mkdir -p $root/gql/api
 cd $root/ent && go run github.com/facebook/ent/cmd/entc generate ./schema &&\
 cd $root/gql && go run github.com/99designs/gqlgen &&\
+if ! which gqlgen_data_gen
+then
+  cd / && go get -v github.com/sunfmin/gqlgen_data_gen
+fi
+
 cd $root/gql && gqlgen_data_gen > ./api/data_gen.go && go fmt api/data_gen.go
