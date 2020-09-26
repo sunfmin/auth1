387fb061073c7e5568cb4b16c83e75c3283c1ce8
hdwswswws <hd0728@qq.com>
2020-09-26 22:35:52 +0800

    add InitAuth and ChangePassword

--- gql/resolver.go
+++ gql/resolver.go
@@ -6,16 +6,18 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
-	"time"
 	"strconv"
-	"gopkg.in/gomail.v2"
-	 masker "github.com/ggwhite/go-masker"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/dysmsapi"
+	"github.com/dgrijalva/jwt-go"
+	masker "github.com/ggwhite/go-masker"
 	"github.com/google/uuid"
 	"github.com/sunfmin/auth1/ent"
 	"github.com/sunfmin/auth1/ent/user"
 	"github.com/sunfmin/auth1/gql/api"
 	"golang.org/x/crypto/bcrypt"
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/dysmsapi"
+	"gopkg.in/gomail.v2"
 )
 
 type Resolver struct {
@@ -23,27 +25,61 @@ type Resolver struct {
 	Config    *api.BootConfig
 }
 
+const (
+	JwtSecretKey    = "welcomelogin"
+	JwtExpireSecond = 3600
+)
+
+func CreateAccessToken(name string) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Username": name,
+		"exp":      time.Now().Add(time.Second * JwtExpireSecond).Unix(),
+	})
+
+	return token.SignedString([]byte(JwtSecretKey))
+}
+func CreateIdToken(id string) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"ID":  id,
+		"exp": time.Now().Add(time.Second * JwtExpireSecond).Unix(),
+	})
+
+	return token.SignedString([]byte(JwtSecretKey))
+}
+func ParseJwtToken(s string) (jwt.MapClaims, error) {
+	fn := func(token *jwt.Token) (interface{}, error) {
+		return []byte(JwtSecretKey), nil
+	}
+	result, error := jwt.Parse(s, fn)
+	if error != nil {
+		return nil, error
+	}
+	finToken := result.Claims.(jwt.MapClaims)
+	return finToken, nil
+}
 func SendMail(mailTo []string, subject string, body string) error {
-  
+
 	mailConn := map[string]string{
-		"user": "hd0728@qq.com", 
-		"pass": "填授权码", 
-		"host": "smtp.qq.com", 
-		"port": "465",  
+		"user": "hd07**@qq.com",
+		"pass": "填授权码",
+		"host": "smtp.qq.com",
+		"port": "465",
 	}
 	port, _ := strconv.Atoi(mailConn["port"])
-  
+
 	m := gomail.NewMessage()
 	m.SetHeader("From", m.FormatAddress(mailConn["user"], "验证码"))
-	m.SetHeader("To", mailTo...)   
-	m.SetHeader("Subject", subject) 
-	 m.SetBody("text/html", body)    
-  
+	m.SetHeader("To", mailTo...)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+
 	d := gomail.NewDialer(mailConn["host"], port, mailConn["user"], mailConn["pass"])
 	err := d.DialAndSend(m)
 	return err
 }
-func DoSendMail(stuEmail , subject, body string) (e error) {
+func DoSendMail(stuEmail, subject, body string) (e error) {
 	mailTo := []string{stuEmail}
 	err := SendMail(mailTo, subject, body)
 	if err != nil {
@@ -52,14 +88,13 @@ func DoSendMail(stuEmail , subject, body string) (e error) {
 	}
 	return nil
 }
-//阿里云的审核还没通过所以没测试
 func SendMsg(tel string, code string) string {
 	client, err := dysmsapi.NewClientWithAccessKey("cn-hangzhou", "<accesskeyId>", "<accessSecret>")
 	request := dysmsapi.CreateSendSmsRequest()
 	request.Scheme = "https"
 	request.PhoneNumbers = tel //手机号变量值
-	request.SignName = "" //签名
-	request.TemplateCode = "" //模板编码
+	request.SignName = ""      //签名
+	request.TemplateCode = ""  //模板编码
 	request.TemplateParam = "{\"code\":\"" + code + "\"}"
 	response, err := client.SendSms(request)
 	fmt.Println(response.Code)
@@ -115,7 +150,10 @@ func (r *mutationResolver) SignUp(ctx context.Context, input api.SignUpInput) (o
 		if err != nil {
 			return
 		}
-		DoSendMail(email,"邮箱验证码",code)
+		if DoSendMail(email, "邮箱验证码", code) != nil {
+			errs := fmt.Errorf("Verification code sending failed")
+			return nil, errs
+		}
 		output = &api.User{CodeDeliveryDetails: &api.CodeDeliveryDetails{AttributeName: "email", DeliveryMedium: "EMAIL", Destination: masker.Mobile(email)}, UserConfirmed: false, UserSub: id.String()}
 		return
 	}
@@ -130,9 +168,9 @@ func (r *mutationResolver) SignUp(ctx context.Context, input api.SignUpInput) (o
 			SetUserAttributes(input.UserAttributes).
 			Save(ctx)
 		if err != nil {
-			return	
+			return
 		}
-		SendMsg(phonenumber,code)
+		SendMsg(phonenumber, code)
 		output = &api.User{CodeDeliveryDetails: &api.CodeDeliveryDetails{AttributeName: "phone_number", DeliveryMedium: "PHONE_NUMBER", Destination: masker.Mobile(phonenumber)}, UserConfirmed: false, UserSub: id.String()}
 		return
 
@@ -141,21 +179,96 @@ func (r *mutationResolver) SignUp(ctx context.Context, input api.SignUpInput) (o
 	return nil, errs
 
 }
-func (r *mutationResolver) ConfirmSignUp(ctx context.Context, input api.ConfirmSignUpInput) (output *api.ConfirmUser, err error) {
+func (r *queryResolver) ConfirmSignUp(ctx context.Context, input api.ConfirmSignUpInput) (output bool, err error) {
 	u, err := r.EntClient.User.Query().Where(user.Username(input.Username)).Only(ctx)
 	if err != nil {
 		return
 	}
-	err = bcrypt.CompareHashAndPassword([]byte(u.ConfirmationCodeHash), []byte(input.ConfirmationCode)) 
+	err = bcrypt.CompareHashAndPassword([]byte(u.ConfirmationCodeHash), []byte(input.ConfirmationCode))
 	if err != nil {
-		output=&api.ConfirmUser{ConfirmUser:false}
-		return output,nil
+		output = false
+		return output, nil
 	}
-	output=&api.ConfirmUser{ConfirmUser:true}
-	return 
+	output = true
+	return
+}
+func (r *queryResolver) InitiateAuth(ctx context.Context, input api.InitiateAuthInput) (output *api.AuthenticationResult, err error) {
+	if input.AuthFlow == "" {
+		errs := fmt.Errorf("AuthFlow is nil")
+		return nil, errs
+	}
+	if input.AuthFlow != "USER_PASSWORD_AUTH" && input.AuthFlow != "EMAIL_PASSWORD_AUTH" && input.AuthFlow != "PHONENUMBER_PASSWORD_AUTH" {
+		errs := fmt.Errorf("Unknow AuthFlow")
+		return nil, errs
+
+	}
+	if input.AuthFlow == "USER_PASSWORD_AUTH" {
+		u, err := r.EntClient.User.Query().Where(user.Username(input.AuthParameters.Username)).Only(ctx)
+		if err != nil {
+			return nil, err
+		}
+		err = bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(input.AuthParameters.Password))
+		if err != nil {
+			return nil, err
+		}
+		AccessToken, err := CreateAccessToken(input.AuthParameters.Username)
+		Idtoken, err := CreateIdToken(u.ID.String())
+		output = &api.AuthenticationResult{AccessToken: AccessToken, ExpiresIn: JwtExpireSecond, IDToken: Idtoken, RefreshToken: "", TokenType: ""}
+		return output, nil
+	}
+	if input.AuthFlow == "EMAIL_PASSWORD_AUTH" {
+		u, err := r.EntClient.User.Query().Where(user.Email(input.AuthParameters.Username)).Only(ctx)
+		if err != nil {
+			return nil, err
+		}
+		err = bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(input.AuthParameters.Password))
+		if err != nil {
+			return nil, err
+		}
+		AccessToken, err := CreateAccessToken(input.AuthParameters.Username)
+		Idtoken, err := CreateIdToken(u.ID.String())
+		output = &api.AuthenticationResult{AccessToken: AccessToken, ExpiresIn: JwtExpireSecond, IDToken: Idtoken, RefreshToken: "", TokenType: ""}
+		return output, nil
+	}
+	u, err := r.EntClient.User.Query().Where(user.PhoneNumber(input.AuthParameters.Username)).Only(ctx)
+	if err != nil {
+		return
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(input.AuthParameters.Password))
+	if err != nil {
+		return
+	}
+	AccessToken, err := CreateAccessToken(input.AuthParameters.Username)
+	Idtoken, err := CreateIdToken(u.ID.String())
+	output = &api.AuthenticationResult{AccessToken: AccessToken, ExpiresIn: JwtExpireSecond, IDToken: Idtoken, RefreshToken: "", TokenType: ""}
+	return
 }
 func (r *queryResolver) GetUser(ctx context.Context, accessToken string) ([]*api.User, error) {
 	panic("not implemented")
+}
+func (r *mutationResolver) ChangePassword(ctx context.Context, input api.ChangePasswordInput) (output bool, err error) {
+	if input.AccessToken == "" {
+		err := fmt.Errorf("AccessToken is nil")
+		return false, err
+	}
+	result, err := ParseJwtToken(input.AccessToken)
+	if err != nil {
+		return
+	}
+	u, err := r.EntClient.User.Query().Where(user.Username(result["Username"].(string))).Only(ctx)
+	if err != nil {
+		return
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(input.PreviousPassword))
+	if err != nil {
+		return
+	}
+	password_hash, err := bcrypt.GenerateFromPassword([]byte(input.ProposedPassword), bcrypt.DefaultCost)
+	_, err = r.EntClient.User.Update().Where(user.Username(result["Username"].(string))).SetPasswordHash(string(password_hash)).Save(ctx)
+	if err != nil {
+		return
+	}
+	return true, nil
 }
 
 // Mutation returns MutationResolver implementation.
